982fc697443374b294a841f86d491ab3af05493a
magi <magali.gonzalez0908@gmail.com>
2024-10-02 18:37:19 -0300

    maquetado y diseño

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'to': 'hsl(0, 74%, 74%)',
+        'from': 'hsl(0, 80%, 86%)',
+        'DesaturatedRed': 'hsl(0, 36%, 70%)',
+        'SoftRed': 'hsl(0, 93%, 68%)',
+      }
+    },
+  },
+  plugins: [],
+}
+
